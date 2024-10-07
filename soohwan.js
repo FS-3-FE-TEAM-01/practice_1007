391169c87eedcce0b611f817dde0cb68601d5e0f
soohwanpak <qoiuytnghks4@gmail.com>
2024-10-07 17:18:43 +0900

    박수환1007

---
+++ soohwan.js
@@ -0,0 +1,252 @@
+export function soohwan(increment) {
+    console.log("박수환");
+    const test = setInterval(() => {
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+        for (let i = 0; i < 100; i++) {
+            increment();
+        }
+    }, 150);
+
+    setTimeout(() => {
+        clearInterval(test);
+    }, 5000);
+}
